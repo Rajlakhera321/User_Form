c2243c9c58d3c8267b4e0182a301502dd2b181d3
Rajlakhera321 <rajlakhera795@gmail.com>
2023-03-12 16:06:47 +0530

    feat: changes in cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require("cors")
 const app = express()
+app.use(cors());
 app.use(express.json())
 const bodyParser = require('body-parser')
 const routes = require('./src/router')
